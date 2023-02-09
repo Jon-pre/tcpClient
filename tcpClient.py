e72870f219d2acfec22f58f1d46c2bb543cd4716
Jon-pre <j-pre@online.no>
2023-02-09 10:25:17 +0100

    Initial commit

---
+++ tcpClient.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+from socket import *
+
+hostname = '127.0.0.1'
+serverPort = 12000
+clientsocket = socket(AF_INET, SOCK_STREAM)
+clientsocket.connect((hostname, serverPort))
+while True:
+    sentence = input("Skriv inn noe her: ")
+    clientsocket.send(sentence.encode())
+    modifiedSentence = clientsocket.recv(1024)
+    print("From server: ", modifiedSentence.decode())
+    if modifiedSentence.decode() == "EXIT":
+        clientsocket.close()
+
+
